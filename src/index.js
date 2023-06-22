7627dd0927fdfc4514529115ac9e4cdc9a92858c
distanceOn <distanceOn@yandex.ru>
2023-06-22 13:10:46 +0300

    Create post add user and delete user methods

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import fs from "fs";
+import { v4 as uuidv4 } from "uuid";
 
 const app = express();
 app.use(express.json());
@@ -28,6 +29,53 @@ app.get("/users/:user_id", (req, res) => {
       const user = users.find((u) => u.id === userId);
       if (user) {
         res.json(user);
+      } else {
+        res.sendStatus(404);
+      }
+    }
+  });
+});
+
+app.post("/users", (req, res) => {
+  const { name } = req.body;
+  fs.readFile(usersFilePath, (err, data) => {
+    if (err) {
+      res.sendStatus(500);
+    } else {
+      const users = JSON.parse(data);
+      const newUser = { id: uuidv4(), name, books: [] };
+      users.push(newUser);
+      fs.writeFile(usersFilePath, JSON.stringify(users), (err) => {
+        if (err) {
+          res.sendStatus(500);
+        } else {
+          res.status(201).json(newUser);
+        }
+      });
+    }
+  });
+});
+
+app.delete("/users/:user_id", (req, res) => {
+  const userId = req.params.user_id;
+
+  fs.readFile(usersFilePath, (err, data) => {
+    if (err) {
+      res.sendStatus(500);
+    } else {
+      let users = JSON.parse(data.toString());
+      const userIndex = users.findIndex(
+        (u) => u.id.toString() === userId.toString()
+      );
+      if (userIndex !== -1) {
+        users.splice(userIndex, 1);
+        fs.writeFile(usersFilePath, JSON.stringify(users), (err) => {
+          if (err) {
+            res.sendStatus(500);
+          } else {
+            res.sendStatus(204);
+          }
+        });
       } else {
         res.sendStatus(404);
       }
